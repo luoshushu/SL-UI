0e096eef99ba33dd6929c8d40870d3cc78a1a0e7
luoshushu <852835916@qq.com>
2019-07-10 22:40:30 +0800

    新增button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import icon from './icon'
+import buttonGroup from './button-group'
 
 Vue.component('sl-button',Button)
 Vue.component('sl-icon',icon)
+Vue.component('sl-button-group',buttonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sl-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+.sl-button-group{
+  display: inline-flex;
+  vertical-align: top;
+  > .sl-button{
+    border-radius: 0;
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+  }
+
+}
+
+</style>
