57d927049edaa70a1a59acdff572a7f23d4aa59f
luoshushu <852835916@qq.com>
2019-07-15 21:46:42 +0800

    添加单元测试chai

--- src/app.js
+++ src/app.js
@@ -14,3 +14,20 @@ new Vue({
     loading2:false,
   }
 });
+
+import chai from 'chai'
+const expect  = chai.expect
+{  
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData:{
+      icon:'previous'
+    }
+  }) 
+  button.$mount('#test') //动态成功按钮 挂载到#test div上
+  let useElement =  button.$el.querySelector('use') //找button中的 use元素
+  console.log(useElement);
+  let href=  useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-previous')
+
+}
