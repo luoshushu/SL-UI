769a5cfaaef169ae44f5dd6fd19215a29e4d8e84
luoshushu <852835916@qq.com>
2019-07-15 22:17:03 +0800

    添加3个测试用例

--- src/app.js
+++ src/app.js
@@ -17,17 +17,88 @@ new Vue({
 
 import chai from 'chai'
 const expect  = chai.expect
+// 单元测试
 {  
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData:{
       icon:'previous'
     }
   }) 
-  button.$mount('#test') //动态成功按钮 挂载到#test div上
-  let useElement =  button.$el.querySelector('use') //找button中的 use元素
+  vm.$mount('#test') //动态成功按钮 挂载到#test div上
+  let useElement =  vm.$el.querySelector('use') //找button中的 use元素
   console.log(useElement);
   let href=  useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-previous')
+  expect(href).to.eq('#i-previous') //期待href 等于#i-previous
+}
+// 测试loading
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'previous',
+      loading:true
+    }
+  }) 
+  vm.$mount() //动态成功按钮 挂载到内存上
+  let useElement =  vm.$el.querySelector('use') //找button中的 use元素
+  console.log(useElement);
+  let href=  useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading') //期待href 等于#i-previous
+    // 成功之后清空button
+    vm.$el.remove() //删除
+    vm.$destroy() //销毁实例
+}
 
+// 测试order
+{
+ const div = document.createElement('div') 
+ document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'previous'
+    }
+  }) 
+  vm.$mount(div) //必须挂载到页面上 否则使用不了order 
+  let svg =  vm.$el.querySelector('svg') //找button中的 use元素
+  let {order}  = window.getComputedStyle(svg)
+  expect(order).to.eq('1') //期待href 等于#i-previous
+  // 成功之后清空button
+  vm.$el.remove() //删除
+  vm.$destroy() //销毁实例
+} 
+{
+ const div = document.createElement('div') 
+ document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'previous',
+      iconPosition:'right'
+    }
+  }) 
+  vm.$mount(div) //必须挂载到页面上 否则使用不了order 
+  let svg =  vm.$el.querySelector('svg') //找button中的 use元素
+  let {order}  = window.getComputedStyle(svg)
+  expect(order).to.eq('2') //期待href 等于#i-previous
+  // 成功之后清空button
+  vm.$el.remove() //删除
+  vm.$destroy() //销毁实例
+}
+// 测试点击 click
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'previous',
+      iconPosition:'right'
+    }
+  }) 
+  vm.$mount() 
+  vm.$on('click',function (params) {
+    console.log(1);
+    
+  })
+  let button 
 }
