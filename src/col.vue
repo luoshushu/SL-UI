f7f28cc92a45ab6379e7fcfd37c7f3d78cf603a8
luoshushu <852835916@qq.com>
2019-08-12 23:30:14 +0800

    布局支持span  offset gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`]">
+
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+  :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+  >
+  <div style="  height: 100px;
+  background: darkolivegreen;  border: 1px solid red;">
     <slot></slot>
+  </div>
   </div>
 </template>
 <script>
@@ -8,21 +14,34 @@ export default {
   props:{
     span:{
       type:[String,Number]
+    },
+    offset:{
+     type:[String,Number]
     }
-  }
+  },
+  data() {
+    return {
+      gutter:0
+    }
+  },
 }
 </script>
 <style lang="scss" scoped>
 .col{
-  height: 100px;
-  background: darkolivegreen;
+
   width: 50%;
-  border: 1px solid red;
+ 
 
   $class-prefix:col-; //css前缀
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
+    }
+  }
+  $class-prefix:offset-; //css前缀
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight:-gutter/2+'px'}" >
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
-  
+  props:{
+    gutter:{
+      type:[String,Number]
+    }
+  },
+  mounted() {
+    this.$children.forEach((vm)=>{
+      vm.gutter = this.gutter
+    })
+    
+  },
 }
 </script>
 
