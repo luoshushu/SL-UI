cbc091c39d884d48d7a798ea9985e1bb672f9dba
susonglin <susonglin@deepexi.com>
2020-05-19 23:26:09 +0800

    feat: 文档雏形

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'SL-UI',
+  description: '苏宋霖的个人造UI组件',
+  themeConfig: {
+    sidebar: [
+      '/',
+      // '/home/',
+      '/install/',
+      '/get-started/'
+    ]
+  }
+}
